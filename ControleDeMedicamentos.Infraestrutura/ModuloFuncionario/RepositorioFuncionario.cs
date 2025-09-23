4bcda9e2ff926a458d219f31e57b466e96e4db50
Calebe <calebepfinger@gmail.com>
2025-09-23 20:45:07 -0300

    Implementado o funcionamento do Modulo Founcionário

---
+++ ControleDeMedicamentos.Infraestrutura/ModuloFuncionario/RepositorioFuncionario.cs
@@ -0,0 +1,7 @@
+﻿using ControleDeMedicamentos.Dominio.ModuloFuncionario;
+using ControleDeMedicamentos.Infraestrutura.Memoria.Compartilhado;
+
+namespace ControleDeMedicamentos.Infraestrutura.Memoria.ModuloFuncionario
+{
+    public class RepositorioFuncionario : RepositorioBase<Funcionario>;
+}
